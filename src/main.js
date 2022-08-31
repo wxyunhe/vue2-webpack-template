448de87e10331261e2db0df8c41cc6bce03eb018
iceytea101 <liyunheasap@yeah.net>
2022-09-01 02:14:27 +0800

    feat: 添加状态管理和路由库

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from "vue";
-import App from "./App";
+import Vue from 'vue';
+import App from './App';
+
+import pinia from './store';
 
 new Vue({
-  el: "#app",
+  el: '#app',
   render(h) {
     return h(App);
   },
+  pinia,
 });
